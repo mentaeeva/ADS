83295d5da5bc516990a01fbf41ca0d3375d61250
Menta Eeva <mentaeeva@gmail.com>
2025-04-09 15:59:55 +0500

    ?? по счету коммит

--- src/WEEK1/MyArrayList.java
+++ src/WEEK1/MyArrayList.java
@@ -68,6 +68,68 @@ public class MyArrayList<T> implements MyList<T> {
         return (T) array[size - 1];
     }
 
+    @Override
+    public void remove (int index){
+        if (index < 0 || index >= size) return;
+        array[index] = null;
+    }
+    @Override
+    public void removeFirst(){
+        if (size == 0) return;
+        size--;
+        array[0] = null;
+    }
+    @Override
+    public void removeLast(){
+        if (size == 0) return;
+        size--;
+        array[size - 1] = null;
+    }
+    @Override
+
+    public void sort() {
+        for (int i = 0; i < size - 1; i++) { // Внешний цикл (проходы по массиву)
+            for (int j = 0; j < size - 1 - i; j++) { // Внутренний цикл (сравнение элементов)
+                Comparable<T> current = (Comparable<T>) array[j]; // Текущий элемент
+                T next = (T) array[j + 1]; // Следующий элемент
+                if (current.compareTo(next) > 0) { // Сравниваем элементы
+                    T temp = (T) array[j]; // Сохраняем текущий элемент
+                    array[j] = array[j + 1]; // Меняем местами
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int indexOf(Object object){
+        for (int i = 0; i < size; i++) {
+            if (object.equals(array[i])) return i ;
+        }
+        return -1;
+
+    }
+    @Override
+    public int lastIndexOf(Object object){
+        for (int i=size-1; i>=0;i--){
+            if (object.equals(array[i])) return i;
+        }
+        return -1;
+    }
+    @Override
+    public boolean exists(Object object){
+        for (int i=0; i<size-1;i++){
+            if (object.equals(array[i])) return true;
+        }
+        return false;
+    }
+    @Override
+    public Object[] toArray(){
+        Object[] ExactArray = new Object[size];
+        for (int i=0;i<size;)
+    }
+
+
 
 
 
